f4514d1b2cba9fc0b3369604e7c52ac934549713
Fabien Hoarau <hoarauf4@gmail.com>
2025-08-16 11:54:18 +0200

    WIP sécurité

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,4 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "intl-tel-input", to: "https://cdn.jsdelivr.net/npm/intl-tel-input@17.0.8/build/js/intlTelInput.min.js", preload: true
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   get 'home/index'
 
   get "/contact", to: "pages#contact"
-  post "/contacts", to: "contacts#create", as: :contacts
+  post "/contact", to: "pages#create_contact", as: :contacts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
