5c87474bba2d8a89c331ce1005cc7d8f8f90c57b
Fabien Hoarau <hoarauf4@gmail.com>
2025-08-22 12:30:36 +0200

    home ok

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "bootstrap"              // charge Bootstrap JS
+import "@popperjs/core"         // ou inclus automatiquement par bootstrap
 
 const emojiFlag = iso2 =>
   iso2
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,4 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "intl-tel-input", to: "https://cdn.jsdelivr.net/npm/intl-tel-input@17.0.8/build/js/intlTelInput.min.js", preload: true
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.2/dist/js/bootstrap.esm.js"
