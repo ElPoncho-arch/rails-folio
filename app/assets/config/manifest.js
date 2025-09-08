44329a785b2159789b167c54c61aeea3505dcbad
Fabien Hoarau <hoarauf4@gmail.com>
2025-09-08 14:07:26 +0200

    Fix Sprockets manifest for importmap (images, styles, builds)

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,3 @@
-//= link application.js
 //= link_tree ../images
-//= link_tree ../builds
 //= link_directory ../stylesheets .css
-//= link_tree ../../javascript/controllers .js
+//= link_tree ../builds
