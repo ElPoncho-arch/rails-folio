7801ae53af80d40b9aba4091853e2e8b58a2bf7e
Fabien Hoarau <hoarauf4@gmail.com>
2025-09-08 11:51:45 +0200

    V1 Heroku: remove videos from Sprockets manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,8 +2,8 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript/controllers .js
 //= link_tree ../../javascript .js
-//= link_tree ../videos
+////= link_tree ../videos
 
-//= link slides_logo_anim.mp4
+////= link slides_logo_anim.mp4
 //= link application.js
 //= link application.css
