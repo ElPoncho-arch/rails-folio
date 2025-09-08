362b453151336783fe710aaf3d28bd768b532d7c
Fabien Hoarau <hoarauf4@gmail.com>
2025-09-08 11:59:45 +0200

    Remove videos from Sprockets manifest (no mp4)

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,9 +1,3 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= link_tree ../../javascript/controllers .js
-//= link_tree ../../javascript .js
-////= link_tree ../videos
-
-////= link slides_logo_anim.mp4
 //= link application.js
-//= link application.css
